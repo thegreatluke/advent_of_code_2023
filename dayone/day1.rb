e49a274e6923a4437d84140a2216dc7fa596cfa9
Luke Rawilns <luke@lukerawlins.com>
2023-12-06 22:32:02 -0500

    hash go brrrrr

--- dayone/day1.rb
+++ dayone/day1.rb
@@ -1,22 +1,24 @@
 #!/usr/bin/env ruby
 
 words = {
-  "eightwo" => "82",
-  "twone"   => "21",
-  "oneight" => "18",
-  "one"     => "1",
-  "two"     => "2",
-  "three"   => "3",
-  "four"    => "4",
-  "five"    => "5",
-  "six"     => "6",
-  "seven"   => "7",
-  "eight"   => "8",
-  "nine"    => "9",
+  "eightwo"   => "82",
+  "twone"     => "21",
+  "oneight"   => "18",
+  "nineeight" => "98",
+  "fivenineeightwo" => "5982",
+  "one"       => "1",
+  "two"       => "2",
+  "three"     => "3",
+  "four"      => "4",
+  "five"      => "5",
+  "six"       => "6",
+  "seven"     => "7",
+  "eight"     => "8",
+  "nine"      => "9",
 }
 
-#lines = File.readlines("cords.txt")
-lines = File.readlines("test_data.txt")
+lines = File.readlines("cords.txt")
+#lines = File.readlines("test_data.txt")
 sum = 0
 
 lines.each do |line|
@@ -24,9 +26,9 @@ lines.each do |line|
   num   = text.gsub(/#{words.keys.join('|')}+/) { |match| words[match] || match }
   #puts num
   nums  = num.gsub(/\D+/) { |match| }
-  puts nums
+  #puts nums
   cords = nums.split(//)
   sum   = "#{cords.first}#{cords.last}".to_i + sum
-  #puts "#{cords.first}#{cords.last}".to_i
+  puts "#{cords.first}#{cords.last}".to_i
 end
-puts sum
+puts "The total is: #{sum}"
